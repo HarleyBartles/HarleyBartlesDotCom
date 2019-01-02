c4ddc234218ff6434c5ba3d0350aa6c69d997eb3
Harley <hbartles81@gmail.com>
2019-01-02 19:06:13 +0000

    Initial

---
+++ HBDotCom/HBDotCom/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HBDotCom.Models;
+
+namespace HBDotCom.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Everything you never wanted to know.";
+
+            return View();
+        }
+
+        public IActionResult AboutSite()
+        {
+            ViewData["Message"] = "What is this site?";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HBDotCom/HBDotCom/wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+
+    var headerSize = $('nav.navbar.navbar-default.navbar-fixed-top').outerHeight(true);
+    var headerSpacers = $('.header.sizer');
+
+    headerSpacers.each(function(){
+        this.outerHeight = headerSize;
+    });
+
+    new WOW().init();
+
+    //$('#owl-carousel').owlCarousel();
+    var $owl = $('#owl-carousel'); 
+
+    if ($owl.length > 0) {
+
+        // Hide the overlays ready for wow to reveal them.
+        $(".owl-overlay:not(:first) .wow").hide();
+
+        $owl.owlCarousel(
+            {
+                items: 1,
+                navigation: true, // Show next and prev buttons
+                autoplay: true,
+                loop: true,
+                slideSpeed: 300,
+                paginationSpeed: 1000,
+                singleItem: true
+            });
+
+        //all other is the same
+        $owl.on('translated.owl.carousel',
+            function (event) {
+                $(".active .wow").addClass("animated").show();
+                $(".owl-item:not(.active) .wow").hide();//.removeClass("fadeInUp");
+            });
+    }
+
+    
+   
+});
