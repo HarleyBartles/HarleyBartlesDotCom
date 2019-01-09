d40dc71f1cd1cff3a6844409d48f334b74dffc7e
Harley <hbartles81@gmail.com>
2019-01-09 13:05:28 +0000

    https port

--- HBDotCom/HBDotCom/Program.cs
+++ HBDotCom/HBDotCom/Program.cs
@@ -19,7 +19,7 @@ namespace HBDotCom
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseSetting("https_port", "443")
+                .UseSetting("https_port", "8080")
                 .UseStartup<Startup>();
     }
 }
