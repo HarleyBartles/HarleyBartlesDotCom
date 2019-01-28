2024bb0bf973972512e99d6c3da6bb4b84cc5a6f
harley bartles <hbartles81@gmail.com>
2019-01-28 11:31:27 +0000

    TwitterTools Initial - search link creation via Ajax.

---
+++ HBDotCom/HBDotCom/Controllers/ToolsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HBDotCom.Controllers
+{
+    public class ToolsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string MakeSearchLink(string userName)
+        {
+            userName = userName.Replace("@", "");
+            return "https://twitter.com/search?from:" + userName + "%20-filter:replies";
+        }
+    }
+}
--- HBDotCom/HBDotCom/wwwroot/js/site.js
+++ HBDotCom/HBDotCom/wwwroot/js/site.js
@@ -1,3 +1,33 @@
 ﻿$(function () {
     new WOW().init();
+
+    var clipboard = new ClipboardJS('.btn');
+
+    clipboard.on('success', function (e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+        toastr.info("Copied")
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function (e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
+
+    $('#MakeLink').on('click', function () {
+        var name = $('#TwitterName').val();
+        $.ajax({
+            url: "/Tools/MakeSearchLink",
+            data: {
+                userName: name
+            }
+        }).done(function (data) {
+            var output = $('#SearchLink');
+            var copyBtn = $('#CopyLink');
+            $(output).val(data);
+            $('.col.invisible').removeClass('invisible');
+        });
+    });
 });
